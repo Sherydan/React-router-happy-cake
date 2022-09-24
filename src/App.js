d0b431f7aac48395b6264c210a0d86d219469915
Sherydan <tobar.luis.c@gmail.com>
2022-09-23 22:47:50 -0300

    added not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -14,6 +15,10 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<About />} />
                     <Route exact path="/contact" element={<Contact />} />
+                    {/* 404 not found route */}
+                    <Route path="*" element={<NotFound />} />
+                    
+                    
                 </Routes>
                 <Footer />
             </Router>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '10rem'
+    }}>
+        <h1 style={{
+            fontSize: '10rem',
+        }}>404</h1>
+        <h2>Page not found</h2>
+       
+
+    </div>
+  )
+}
+
+export default NotFound
